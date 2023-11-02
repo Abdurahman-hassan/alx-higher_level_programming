08e3a1c36d77c4f486fe717975d6578e21b1f9e9
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-02 23:36:22 +0200

    enhance in print statment add more conditions

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,17 @@ import sys
 
 def main():
     """main function"""
+    num_of_args = len(sys.argv) - 1
 
-    print("{} arguments".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+    if num_of_args == 0:
+        print("{} arguments.".format(num_of_args))
+    elif num_of_args == 1:
+        print("{} argument:".format(num_of_args))
+        print("{}: {}".format(num_of_args, sys.argv[1]))
+    else:
+        print("{} arguments:".format(num_of_args))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
 
 
 if __name__ == "__main__":
