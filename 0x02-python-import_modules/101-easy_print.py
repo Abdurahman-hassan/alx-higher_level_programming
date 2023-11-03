f3821197743342e721bd8475ae9d470871ab29e8
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-03 04:16:05 +0200

    fix linting

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,2 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 #another way# __import__('os').system('echo \#pythoniscool')
 getattr(__import__('sys'), 'stdout').write('#pythoniscool\n')
-# another way # __import__('os').system('echo \#pythoniscool')
