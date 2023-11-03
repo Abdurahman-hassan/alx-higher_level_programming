c0c2ef3ba5214c6351085d2fc72e0b3b9199a77e
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-03 04:18:25 +0200

    add another way

--- 0x02-python-import_modules/101-easy-print2.py
+++ 0x02-python-import_modules/101-easy-print2.py
@@ -1,1 +1,2 @@
-# another way# __import__('os').system('echo \#pythoniscool')
+# another way to print without using print or eval or open or import sys
+__import__('os').system('echo \#pythoniscool')
