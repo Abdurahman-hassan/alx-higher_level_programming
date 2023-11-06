fae50736e4874bc51bd3796d1d4476733bdf2605
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-07 01:37:24 +0200

    change direct loop with index loop

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        return
     for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" ")
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end=" ")
         print()
