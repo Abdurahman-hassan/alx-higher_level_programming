feaca5a6baae45392d8f1ae94d4dff1722ffdd8c
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-09 00:25:38 +0200

    add docstring

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
+    """Computes the square value of all integers of a matrix."""
     new_matrix = []
     for row in matrix:
         # new_matrix.append([x ** 2 for x in row])
