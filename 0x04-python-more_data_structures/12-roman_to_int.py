0033bef1406545a18162f2f1ffe64c9f3dfd7d71
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-09 02:31:51 +0200

    add comments again

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -15,3 +15,34 @@ def roman_to_int(roman_string):
         else:
             result += roman_values[roman_string[i]]
     return result
+
+# clarifications on this line:
+# roman_values[roman_string[i]] is the value of the current roman numeral
+# roman_values[roman_string[i - 1]] is the value of the previous roman numeral
+# roman_values[roman_string[i]] - 2 * roman_values[roman_string[i - 1]]
+# is the value of the current roman numeral
+# minus twice the value of the previous roman numeral
+# this is because the previous roman numeral
+# was already added to the result in the previous iteration
+# so we need to subtract it twice to get the correct value
+
+# example: roman_string = "IV"
+# the first iteration will add 1 to the result
+# the second iteration will add 5 to the result
+# but we need to subtract 1 twice to get the correct value
+# so we subtract 1 twice from 5 to get 3
+# and we add 3 to the result
+
+# example: roman_string = "IX"
+# the first iteration will add 1 to the result
+# the second iteration will add 10 to the result
+# but we need to subtract 1 twice to get the correct value
+# so we subtract 1 twice from 10 to get 8
+# and we add 8 to the result
+
+# example: roman_string = "XL"
+# the first iteration will add 10 to the result
+# the second iteration will add 50 to the result
+# but we need to subtract 10 twice to get the correct value
+# so we subtract 10 twice from 50 to get 30
+# and we add 30 to the result
