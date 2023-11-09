c41be68d79cd4536310d9e615b0e82d0d04c254d
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-09 03:22:07 +0200

    fix linting

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,11 +5,13 @@ def weight_average(my_list=[]):
     if my_list == []:
         return 0
     else:
-        return sum([x[0] * x[1] for x in my_list]) / sum([x[1] for x in my_list])
+        return (sum([x[0] * x[1] for x in my_list])
+                / sum([x[1] for x in my_list]))
 
 # we can also use the map function to do the same thing
 # def weight_average(my_list=[]):
 #     if my_list == []:
 #         return 0
 #     else:
-#         return sum(map(lambda x: x[0] * x[1], my_list)) / sum(map(lambda x: x[1], my_list))
+#         return sum(map(lambda x: x[0] * x[1], my_list))
+#         / sum(map(lambda x: x[1], my_list))
