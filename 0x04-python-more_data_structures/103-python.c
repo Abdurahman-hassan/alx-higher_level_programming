e133b9b9a07ac0c78ab8ea052240e6b23b1f3585
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-09 02:00:10 +0000

    change in logic

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,60 +1,71 @@
 #include <Python.h>
+#include <object.h>
 #include <listobject.h>
 #include <bytesobject.h>
 
-/* Function to display information about byte objects in Python */
-void display_python_bytes(PyObject *py_object)
+/*
+ * print_python_bytes - Prints information about Python bytes objects.
+ * @p: PyObject pointer to a Python bytes object.
+ *
+ * This function checks if the given PyObject is a bytes object and,
+ * if so, prints its size, the string it contains, and the first ten bytes
+ * of its contents in hexadecimal format. If the object is not a valid
+ * bytes object, it outputs an error message.
+ */
+void print_python_bytes(PyObject *p)
 {
-	long bytes_size;
-	char *bytes_content = NULL;
+	long int byte_size; /* Size of the bytes object. */
+	int index;          /* Loop index for iterating over bytes. */
+	char *byte_content; /* Pointer to bytes object content. */
 
 	printf("[.] bytes object info\n");
-
-	/* Validate that the object is a bytes object */
-	if (!PyBytes_Check(py_object))
+	if (!PyBytes_Check(p)) /* Check if PyObject is a bytes object. */
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
-	/* Retrieve string and its length from the bytes object */
-	PyBytes_AsStringAndSize(py_object, &bytes_content, &bytes_size);
+	/* Retrieves content and size of bytes object. */
+	PyBytes_AsStringAndSize(p, &byte_content, &byte_size);
 
-	printf("  size: %ld\n", bytes_size);
-	printf("  trying string: %s\n", bytes_content);
+	printf("  size: %li\n", byte_size);
+	printf("  trying string: %s\n", byte_content);
+	printf("  first %li bytes:", byte_size < 10 ? byte_size + 1 : 10);
 
-	/* Print up to 10 bytes of the bytes object */
-	printf("  first %ld bytes:", (bytes_size < 10) ? bytes_size + 1 : 10);
-	for (int index = 0; index <= bytes_size && index < 10; ++index)
-	{
-		printf(" %02hhx", bytes_content[index]);
-	}
+	/* Print up to the first 10 bytes of the bytes object. */
+	for (index = 0; index <= byte_size && index < 10; index++)
+		printf(" %02hhx", byte_content[index]);
 	printf("\n");
 }
 
-/* Function to display information about list objects in Python */
-void display_python_list(PyObject *py_object)
+/*
+ * print_python_list - Prints information about Python list objects.
+ * @p: PyObject pointer to a Python list object.
+ *
+ * This function outputs the size of the list, the allocated memory for it,
+ * and details of each element contained within the list, including their type.
+ * If an element is a bytes object, it calls print_python_bytes to print
+ * further information about the bytes object.
+ */
+void print_python_list(PyObject *p)
 {
-	PyListObject *py_list = (PyListObject *)py_object;
-	long list_size = PyList_Size(py_object);
+	long int list_size = PyList_Size(p); /* Number of items in the list. */
+	PyListObject *list_obj = (PyListObject *)p; /* Cast to PyListObject. */
+	const char *element_type; /* Type name of the list element. */
 
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", list_size);
-	printf("[*] Allocated = %ld\n", py_list->allocated);
+	printf("[*] Size of the Python List = %li\n", list_size);
+	printf("[*] Allocated = %li\n", list_obj->allocated);
 
-	/* Iterate over the list and print details about each element */
-	for (int index = 0; index < list_size; ++index)
+	/* Iterate over each element in the list and print its type. */
+	for (int i = 0; i < list_size; i++)
 	{
-		PyObject *item = PyList_GET_ITEM(py_list, index);
-		const char *item_type = item->ob_type->tp_name;
+		element_type = Py_TYPE(list_obj->ob_item[i])->tp_name;
+		printf("Element %i: %s\n", i, element_type);
 
-		printf("Element %d: %s\n", index, item_type);
-
-		/* If the list element is a bytes object, display its info */
-		if (PyBytes_Check(item))
-		{
-			display_python_bytes(item);
-		}
+		/* If element is a bytes object, call print_python_bytes. */
+		if (PyBytes_Check(list_obj->ob_item[i]))
+			print_python_bytes(list_obj->ob_item[i]);
 	}
 }
 
