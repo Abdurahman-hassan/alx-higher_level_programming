c9030514c54be5983ca84f346f1996d3a513541d
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-22 04:24:11 +0200

    fix linting

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,14 +4,19 @@ import math
 
 
 class MagicClass:
+    """MagicClass: Magic class"""
+
     def __init__(self, radius=0):
+        """__init__: Init method"""
         self.__radius = 0
         if type(radius) not in [int, float]:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
+        """area: Return the area of the circle"""
         return math.pi * self.__radius ** 2
 
     def circumference(self):
+        """circumference: Return the circumference of the circle"""
         return 2 * math.pi * self.__radius
