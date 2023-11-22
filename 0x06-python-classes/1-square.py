77538b1f78f2ca6d7b75a9b3c4917f06c37a62a4
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-22 03:43:33 +0200

    Add  a class Square that defines a square by: (based on 0-square.py)
    Private instance attribute: size
    Instantiation with size (no type/value verification)
    You are not allowed to import any module

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module creates a class Square"""
+
+
+class Square:
+    """This class defines a Square"""
+
+    def __init__(self, size):
+        """Initializes attributes
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
