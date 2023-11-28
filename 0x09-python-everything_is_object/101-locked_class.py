bc7f56cfe7980089c2ac575222f79079120a7c97
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-28 19:30:51 +0200

    add init func

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+"""Defines a locked class."""
+
+
 class LockedClass:
     """Prevents the user from dynamically creating new instance attributes,
     except if the new instance attribute is called first_name.
     """
     __slots__ = ["first_name"]
+
+    def __init__(self, first_name=""):
+        """Initializes the instance attributes."""
+        self.first_name = first_name
