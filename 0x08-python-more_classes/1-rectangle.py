5fbb776020771122623ac2dbf992547d8648e924
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-28 02:13:50 +0200

    change the condition :
    if type(value) is not int:
    
    to
    
    if not isinstance(value, int):

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Method setter for width atributte"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
 
         if value < 0:
@@ -40,7 +40,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Method setter for height atributte"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
 
         if value < 0:
