8f71b70b3c18459e3f63a335fec7c9813379ae82
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-12-05 02:15:58 +0200

    Add import, add class Rectangle and make BaseGeometry inherted from it

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,48 +2,28 @@
 
 
 """Module for BaseGeometry class."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
+class Rectangle(BaseGeometry):
     """
     A base class for geometry-related operations.
     """
 
     def __init__(self, width, height):
         """
-        Initializes a BaseGeometry object.
+        Initializes a Rectangle geometry.
 
         Args:
-            width (int): The width of the object.
-            height (int): The height of the object.
-        """
-
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        Placeholder method for calculating the area of a geometry.
-        This method is meant to be overridden in derived classes.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates that a value is an integer greater than 0.
-
-        Args:
-            name (str): The name of the value being validated.
-            value: The value to be validated.
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
 
         Raises:
             TypeError: If the value is not an integer.
             ValueError: If the value is less than or equal to 0.
         """
 
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
