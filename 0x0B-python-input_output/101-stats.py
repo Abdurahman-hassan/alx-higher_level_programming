519043c008277eae417d030c18b2aae9f5b4e59d
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-12-06 02:43:16 +0200

    udpate the logic

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,58 +3,51 @@
 import sys
 import signal
 
-
 def signal_handler(sig, frame):
-    """ Signal handler for CTRL+C
-
-    Args:
-        sig (int): Signal number.
-        frame (frame): Current stack frame.
-    """
+    """ Signal handler for CTRL+C """
     print_statistics()
     sys.exit(0)
 
-
 def print_statistics():
-    """ Print statistics
-        Prints the total file size and the number
-        of times each HTTP status code
-        appears in the log.
-    """
+    """ Print statistics """
     print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        print("{}: {}".format(code, status_codes[code]))
-
+    for code in sorted(status_codes):
+        if code in valid_codes:
+            print("{}: {}".format(code, status_codes[code]))
 
 # Register the signal handler for CTRL+C
 signal.signal(signal.SIGINT, signal_handler)
 
 # Initialize variables
 total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_codes = {}
 line_count = 0
+valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 
 # Read from stdin
-for line in sys.stdin:
-    try:
-        parts = line.split()
-        status_code = int(parts[-2])
-        file_size = int(parts[-1])
-
-        # Update statistics
-        total_size += file_size
-        if status_code in status_codes:
-            status_codes[status_code] += 1
+try:
+    for line in sys.stdin:
+        if line_count == 10:
+            print_statistics()
+            line_count = 0
 
         line_count += 1
 
-        # Every 10 lines, print statistics
-        if line_count % 10 == 0:
-            print_statistics()
+        try:
+            parts = line.split()
+            status_code = str(int(parts[-2]))  # Ensure it's a valid integer then convert to string
+            file_size = int(parts[-1])
 
-    except Exception as e:
-        # You can handle or print exceptions here if needed
-        pass
+            # Update statistics
+            total_size += file_size
+            if status_code in valid_codes:
+                status_codes[status_code] = status_codes.get(status_code, 0) + 1
 
-# Print final statistics
-print_statistics()
+        except (ValueError, IndexError):
+            pass
+
+    print_statistics()
+
+except KeyboardInterrupt:
+    print_statistics()
+    raise
