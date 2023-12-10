62a2822db2145432c0720c5ca6e0d0ef00c1a71b
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-12-11 01:29:00 +0200

    the class Square that inherits from Rectangle:
    In the file models/square.py
    Class Square inherits from Rectangle
    Class constructor: def __init__(self, size, x=0, y=0, id=None)::
    Call the super class with id, x, y, width and height - this super call will use the logic of the __init__ of the Rectangle class. The width and height must be assigned to the value of size
    You must not create new attributes for this class, use all attributes of Rectangle - As reminder: a Square is a Rectangle with the same width and height
    All width, height, x and y validation must inherit from Rectangle - same behavior in case of wrong data
    The overloading __str__ method should return [Square] (<id>) <x>/<y> - <size> - in our case, width or height
    As you know, a Square is a special Rectangle, so it makes sense this class Square inherits from Rectangle. Now you have a Square class who has the same attributes and same methods.

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""Defines a Square class."""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes a Square instance.
+
+        Args:
+            size (int): The size of the Square instance.
+            x (int): The x coordinate of the Square instance.
+            y (int): The y coordinate of the Square instance.
+            id (int): The identity of the Square instance.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns the string representation of a Square instance."""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """Gets the size attribute of a Square instance."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size attribute of a Square instance.
+
+        args:
+            value (int): The value to set
+            the size attribute to.
+        """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Updates a Square instance.
+        """
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i, e in enumerate(args):
+                setattr(self, attrs[i], e)
+        elif kwargs:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Square instance."""
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
