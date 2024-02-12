9c1e02493f32d3611b9f7c54b1cada04d77a9115
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-13 01:25:38 +0200

    use another way

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const argCount = process.argv.length;
-console.log(argCount === 2 ? 'No argument' : process.argv[2])
+const args = process.argv.slice(2);
+console.log(args[0] || 'No argument');
