80fc60bea069cd0326c4235e933e799903869910
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-13 01:40:38 +0200

    combine the code

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (isNaN(args[0])) {
-    console.log('Not a number');
-} else {
-    console.log('My number: ' + args[0]);
-}
+console.log(isNaN(args[0]) ? 'Not a number' : 'My number: ' + args[0]);
