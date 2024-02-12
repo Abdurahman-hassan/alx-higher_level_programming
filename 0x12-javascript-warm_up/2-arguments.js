90bb8921774dd9e3ea4344ebe6aa45878e4a978b
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-13 01:14:21 +0200

    change variable name to camelcase

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const arg_count = process.argv.length;
-console.log(arg_count === 2 ? 'No argument' : arg_count === 3 ? 'Argument found' : 'Arguments found');
+const argCount = process.argv.length;
+console.log(argCount === 2 ? 'No argument' : argCount === 3 ? 'Argument found' : 'Arguments found');
