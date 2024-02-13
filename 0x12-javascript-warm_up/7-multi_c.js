f586650f51367ada987972b8eef17ff4410a5fd4
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-13 03:26:03 +0200

    change the consition

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let processArg = parseInt(process.argv[2]);
+let number = parseInt(process.argv[2]);
 
-if (process.argv.length === 2 || isNaN(process.argv[2])) {
+if (process.argv.length === 2 || isNaN(number)) {
     console.log('Missing number of occurrences');
 } else {
     for (let i = 0; i < processArg; i++) {
