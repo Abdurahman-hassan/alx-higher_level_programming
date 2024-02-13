ac9c7205581853c64409d9f449fe784503d4a17b
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-13 03:34:11 +0200

    change the structure

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-
-let number = parseInt(process.argv[2]);
-
-if (isNaN(number)) { console.log('Missing number of occurrences'); }
-else {
-    for (; number > 0; number--) { console.log('C is fun'); }
+const num = Math.floor(Number(process.argv[2]));
+if (isNaN(num)) {
+    console.log('Missing number of occurrences');
+} else {
+    for (let i = 0; i < num; i++) {
+        console.log('C is fun');
+    }
 }
