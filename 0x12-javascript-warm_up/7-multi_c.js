3b9ce66d3134a0aeaa90fda4dce5a80086913763
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-13 03:22:32 +0200

    fix linting

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 const argLen = process.argv.length;
+let processArg = parseInt(process.argv[2]);
 
 if (argLen === 2 || isNaN(process.argv[2])) {
     console.log('Missing number of occurrences');
 }
-
-let processArg = parseInt(process.argv[2]);
 
 for (let i = 0; i < processArg; i++) {
     console.log('C is fun');
