5e6fc2ae294f1cf4ede8edd3852b0d3d81574980
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-13 03:16:46 +0200

    a script that prints x times “C is fun”
    Where x is the first argument of the script
    If the first argument can’t be converted to an integer, print “Missing number of occurrences”
    You must use console.log(...) to print all output
    You are not allowed to use var
    You can use only two console.log
    You must use a loop (while, for, etc.)

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const argLen = process.argv.length;
+if (argLen === 2) {
+    console.log('Missing number of occurrences');
+}
+let processArg = parseInt(process.argv[2]);
+for (let i = 0; i < processArg; i++) {
+    console.log('C is fun');
+}
