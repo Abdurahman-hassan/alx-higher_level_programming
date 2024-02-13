27cb4ecb5da66ef5b192fa9bd9e4ebbb50aff24d
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-13 03:59:27 +0200

    a script that prints the addition of 2 integers
    The first argument is the first integer
    The second argument is the second integer
    You have to define a function with this prototype: function add(a, b)
    You must use console.log(...) to print all output
    You are not allowed to use var

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// sum two numbers
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+if (isNaN(a) || isNaN(b)) {
+  console.log('NaN');
+} else {
+  console.log(a + b);
+}
