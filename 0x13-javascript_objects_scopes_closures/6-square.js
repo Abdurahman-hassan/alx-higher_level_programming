4388abf059d2461df84c02243ec2ed566debb1df
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-13 23:45:30 +0200

    a class Square that defines a square and inherits from Square of 5-square.js:
    You must use the class notation for defining your class and extends
    Create an instance method called charPrint(c) that prints the rectangle using the character c
    If c is undefined, use the character X

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const SquareObject = require('./5-square');
+class Square extends SquareObject {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
