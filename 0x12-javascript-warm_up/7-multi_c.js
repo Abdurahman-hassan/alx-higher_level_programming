85c6b791bc209a55a5042b87162cf744cceec0fe
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-13 03:26:46 +0200

    fix the loop

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ let number = parseInt(process.argv[2]);
 if (process.argv.length === 2 || isNaN(number)) {
     console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < processArg; i++) {
+    for (let i = 0; i < number; i++) {
         console.log('C is fun');
     }
 }
