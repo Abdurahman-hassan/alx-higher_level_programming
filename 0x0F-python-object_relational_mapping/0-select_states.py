0058b4e2571e01a9d872ac0048446844cfefa194
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-19 02:51:37 +0200

    a script that lists all states from the database hbtn_0e_0_usa:
    Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
    You must use the module MySQLdb (import MySQLdb)
    Your script should connect to a MySQL server running on localhost at port 3306
    Results must be sorted in ascending order by states.id
    Results must be displayed as they are in the example below
    Your code should not be executed when imported

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
+import sys
+
+import MySQLdb
+
+
+def list_states(username, password, dbname):
+    """
+    Connects to a MySQL database
+    and prints all states in the database.
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=dbname)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        list_states(sys.argv[1], sys.argv[2], sys.argv[3])
