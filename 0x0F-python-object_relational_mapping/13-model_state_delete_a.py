aec75910a6f0f05e62cdbebae7fcffa144825998
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-19 03:48:32 +0200

    a script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa
    Your script should take 3 arguments: mysql username, mysql password and database name
    You must use the module SQLAlchemy
    You must import State and Base from model_state - from model_state import Base, State
    Your script should connect to a MySQL server running on localhost at port 3306
    Your code should not be executed when imported

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+Script to delete all State objects whose names contain the letter 'a'
+from a MySQL database.
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # Create a connection to the MySQL database using the provided credentials
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Fetch all states whose names contain the letter 'a' from the database
+    st_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+
+    for state in st_to_delete:
+        session.delete(state)
+    session.commit()
+
+    session.close()
