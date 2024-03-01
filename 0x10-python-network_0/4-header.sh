d07ca80eed0fee0699326d32535ad68a98af9eb1
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-01 04:30:14 +0200

    a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
    A header variable X-School-User-Id must be sent with the value 98
    You have to use curl

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Sends a GET request to the URL passed as the first argument and displays the body of the response
+# A header variable X-School-User-Id is sent with the value 98
+curl -sX GET -H "X-School-User-Id: 98" "$1"
