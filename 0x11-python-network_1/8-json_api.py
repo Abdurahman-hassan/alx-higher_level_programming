b9c28a02b46b3e546de2a7500d4c92e0cb93b5fc
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-05 20:06:06 +0200

    a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
    The letter must be sent in the variable q
    If no argument is given, set q=""
    If the response body is properly JSON formatted and not empty, display the id and name like this: [<id>] <name>
    Otherwise:
    Display Not a valid JSON if the JSON is invalid
    Display No result if the JSON is empty
    You must use the package requests and sys
    You are not allowed to import packages other than requests and sys
    Please test your script in the sandbox provided, using the web server running on port 5000. All JSON generated by this server are random.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+
+import requests
+
+if __name__ == "__main__":
+    # Set q to the first command line argument
+    # or an empty string if not provided
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    payload = {'q': letter}
+    URL = 'http://0.0.0.0:5000/search_user'
+    try:
+        # Sending a POST request with the letter as data
+        response = requests.post(URL, data=payload)
+
+        # Attempting to parse the JSON response
+        json_response = response.json()
+
+        if json_response:
+            # If there is a response, display the id and name
+            print("[{}] {}".format(json_response.get('id'),
+                                   json_response.get('name')))
+        else:
+            # If the response is empty
+            print("No result")
+    except ValueError:
+        # If there is an error parsing JSON
+        print("Not a valid JSON")
