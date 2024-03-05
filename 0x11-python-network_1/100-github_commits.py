7c9c8f90150625f2879ec49748b627c9c974bfc4
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-05 20:28:36 +0200

    fix linting

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Fetches the 10 most recent commits from the repository specified"""
-import requests
 import sys
 
+import requests
+
 if __name__ == "__main__":
-    URL = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
+    BASE_URL = 'https://api.github.com/repos/'
+    URL = "{}{}/{}/commits".format(BASE_URL, sys.argv[2], sys.argv[1])
     response = requests.get(URL)
     commits = response.json()
     for commit in commits[:10]:
-        print("{}: {}".format(commit.get('sha'), commit.get('commit').get('author').get('name')))
+        print("{}: {}".format(commit.get('sha'),
+                              commit.get('commit').get('author').get('name')))
