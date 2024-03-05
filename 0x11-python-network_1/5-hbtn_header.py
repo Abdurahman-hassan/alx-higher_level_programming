100be9b1c7f277b60e9cb88d07acc68bd6a9a878
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-05 19:27:00 +0200

    use get function insted of ['']

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,4 +6,5 @@ import sys
 if __name__ == "__main__":
     URL = sys.argv[1]
     response = requests.get(URL)
-    print(response.headers['X-Request-Id'])
+    x_request_id = response.headers.get('X-Request-Id')
+    print(x_request_id)
