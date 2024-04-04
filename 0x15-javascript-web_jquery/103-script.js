b8ddba51ba789e92d9d4224ae00e02fe9ad97384
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-04-04 03:04:02 +0200

    a JavaScript script that fetches and prints how to say “Hello” depending on the language
    You should use this API service: https://www.fourtonfish.com/hellosalut/hello/
    The language code will be the value entered in the tag INPUT#language_code (ex: es, fr, en etc.)
    The translation must be fetched when the user clicks on INPUT#btn_translate OR presses ENTER when the focus is on INPUT#language_code
    The translation of “Hello” must be displayed in the HTML tag DIV#hello
    You can’t use document.querySelector to select the HTML tag
    You must use the JQuery API
    You script must work when imported from the <head> tag

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+$('document').ready(function () {
+    function getTranslation () {
+        const langCode = $('INPUT#language_code').val();
+        const url = 'https://hellosalut.stefanbohacek.dev/?';
+
+        $.get(url + $.param({ lang: langCode }), function (data) {
+            $('DIV#hello').html(data.hello);
+        });
+    }
+
+    $('INPUT#btn_translate').click(getTranslation);
+
+    $('#language_code').keypress(function (event) {
+        if (event.which === 13) { // Check if "Enter" key is pressed
+            getTranslation();
+        }
+    });
+});
