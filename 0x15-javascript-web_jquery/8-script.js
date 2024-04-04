8ae73eaaab2c24a606f04f8a92b827e6eaa93933
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-04-04 02:07:57 +0200

    a JavaScript script that fetches and lists the title for all movies by using this URL: https://swapi-api.alx-tools.com/api/films/?format=json
    All movie titles must be list in the HTML tag UL#list_movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+$(document).ready(function () {
+    $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+        $.each(data.results, function (index, movie) {
+            $('UL#list_movies').append('<li>' + movie.title + '</li>');
+        });
+    });
+});
