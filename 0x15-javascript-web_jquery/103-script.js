664997aa85ecd6424acb5f18f633180df486b40c
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-04-04 03:07:52 +0200

    fix linting

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,18 +1,18 @@
 $('document').ready(function () {
-    function getTranslation () {
-        const langCode = $('INPUT#language_code').val();
-        const url = 'https://hellosalut.stefanbohacek.dev/?';
+  function getTranslation () {
+    const langCode = $('INPUT#language_code').val();
+    const url = 'https://hellosalut.stefanbohacek.dev/?';
 
-        $.get(url + $.param({ lang: langCode }), function (data) {
-            $('DIV#hello').html(data.hello);
-        });
-    }
-
-    $('INPUT#btn_translate').click(getTranslation);
-
-    $('#language_code').keypress(function (event) {
-        if (event.which === 13) { // Check if "Enter" key is pressed
-            getTranslation();
-        }
+    $.get(url + $.param({ lang: langCode }), function (data) {
+      $('DIV#hello').html(data.hello);
     });
+  }
+
+  $('INPUT#btn_translate').click(getTranslation);
+
+  $('#language_code').keypress(function (event) {
+    if (event.which === 13) { // Check if "Enter" key is pressed
+      getTranslation();
+    }
+  });
 });
--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
-        $.each(data.results, function (index, movie) {
-            $('UL#list_movies').append('<li>' + movie.title + '</li>');
-        });
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    $.each(data.results, function (index, movie) {
+      $('UL#list_movies').append('<li>' + movie.title + '</li>');
     });
+  });
 });
