0162ce4e05121e603eebbf76ba9895dcb2034df2
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-04-04 02:20:33 +0200

    update the urls

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 $(document).ready(function () {
-    $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+    $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
         $.each(data.results, function (index, movie) {
             $('UL#list_movies').append('<li>' + movie.title + '</li>');
         });
